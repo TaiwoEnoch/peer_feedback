2c4b42619d1af5e376d6370b5cf8322891fbc064
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-03-01 14:37:10 +0100

    fix errors

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  plugins: [new HtmlWebpackPlugin({ 
+  plugins: [new HtmlWebpackPlugin({
     template: './src/index.html',
   })],
   mode: 'development',
